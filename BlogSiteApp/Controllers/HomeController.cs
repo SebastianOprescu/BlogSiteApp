1bfadb075d8523d4cb5767196804c788f57b3799
Me <Me@DESKTOP-G6T2S0A>
2023-03-05 21:44:01 +0200

    Add project files.

---
+++ BlogSiteApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using BlogSiteApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSiteApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DatabaseBlogEntities db = new DatabaseBlogEntities();
+        public ActionResult Index()
+        {
+            return View(db.Blogs.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
